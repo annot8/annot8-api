e2714d3031929f221f17d89c67848bdf8aabdcf7
Stuart Hendren <stuart@committed.io>
2022-01-18 12:30:00 +0000

    Adds isRunning to the Pipeline
    Gives a default so compatible with any existing implementation.
    
    Fixes #9

--- src/main/java/io/annot8/api/pipelines/PipelineRunner.java
+++ src/main/java/io/annot8/api/pipelines/PipelineRunner.java
@@ -2,5 +2,21 @@
 package io.annot8.api.pipelines;
 
 public interface PipelineRunner extends Runnable {
+
+  /**
+   * Signal to stop the pipeline.
+   *
+   * <p>Note, implementations may choose to stop imediately or contine processing the current items
+   * to completion.
+   */
   void stop();
+
+  /**
+   * Check if the pipeline is currently running
+   *
+   * @return true if running (or unable to tell)
+   */
+  default boolean isRunning() {
+    return true;
+  }
 }
